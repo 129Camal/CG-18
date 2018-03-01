cbbb9d584125c21ee0a86a1376bfa0d16e6f03e6
129Camal <frederico21pinto@hotmail.com>
2018-03-01 19:45:18 +0000

    Cone feito

--- gerador/gerador.cpp
+++ gerador/gerador.cpp
@@ -179,6 +179,105 @@ void box(double c, double l, double a, int camadas, string f) {
         zz += espL;
 
     }
+    file.close();
+}
+
+
+void cone(float r, float a, int cV, int cH, string f){
+    ofstream file(f);
+    float x1, x2, x3, y1, y2, y3, z1, z2, z3;
+    float x4, x5, x6, y4, y5, y6, z4, z5, z6;
+    float x7, x8, x9, y7, y8, y9, z7, z8, z9;
+
+    float espV = (2 * M_PI) / cV;
+    float espH = a / cH;
+    float altura = -(a/2); //centrar
+
+    //fazer a circunferência
+    for (float ang = 0; ang < 2 * M_PI; ang+= espV){
+        x1 = 0;
+        y1 = altura;
+        z1 = 0;
+
+        x2 = r * sin(ang + espV);
+        y2 = altura;
+        z2 = r * cos(ang + espV);
+
+        x3 = r * sin(ang);
+        y3 = altura;
+        z3 = r * cos(ang);
+
+        printf("%f %f %f\n", x1, y1, z1);
+        file << x1 << "," << y1 << "," << z1 << endl;
+        printf("%f %f %f\n", x2, y2, z2);
+        file << x2 << "," << y2 << "," << z2 << endl;
+        printf("%f %f %f\n", x3, y3, z3);
+        file << x3 << "," << y3 << "," << z3 << endl;
+
+    }
+
+    //fazer a parte de cima do cone
+    for(int i = 0; i < cH; i++){
+
+        for(int a = 0; a < cV; a += espV){
+
+            float patamar = r + i*espH;
+            float patamarA = r + (i+1) * espH;
+
+            float raioB = r - (r * i);
+            float raioA = r - (r * (i+1));
+
+            x4 = raioA * sin(a);
+            y4 = patamarA;
+            z4 = raioA * cos(a);
+
+            x5 = raioB * sin(a);
+            y5 = patamar;
+            z5 = raioB * cos(a);
+
+            x6 = raioA * sin(a + espV);
+            y6 = patamarA;
+            z6 = raioB * sin(a + espV);
+
+            printf("%f %f %f\n", x4, y4, z4);
+            file << x1 << "," << y1 << "," << z1 << endl;
+            printf("%f %f %f\n", x5, y5, z5);
+            file << x2 << "," << y2 << "," << z2 << endl;
+            printf("%f %f %f\n", x6, y6, z6);
+            file << x3 << "," << y3 << "," << z3 << endl;
+
+            x7 = raioB * sin(a);
+            y7 = patamar ;
+            z7 = raioB * cos(a);
+
+            x8 = raioB * sin(a + espV);
+            y8 = patamar;
+            z8 = raioB * cos(a + espV);
+
+            x9 = raioA * sin(a + espV);
+            y9 = patamarA;
+            z9 = raioB * cos(a + espV);
+
+            printf("%f %f %f\n", x7, y7, z7);
+            file << x7 << "," << y7 << "," << z7 << endl;
+            printf("%f %f %f\n", x8, y8, z8);
+            file << x8 << "," << y8 << "," << z8 << endl;
+            printf("%f %f %f\n", x9, y9, z9);
+            file << x9 << "," << y9 << "," << z9 << endl;
+        }
+
+    }
+    file.close();
+
+
+}
+
+void sphere(float r, int cv, int ch, string f){
+    //ofstream file(f);
+    float espV = 2* M_PI; //camadasV
+    float espH = M_PI;  //camadasH
+
+
 }
 
 
@@ -192,5 +291,5 @@ int main(int argc, char **argv) {
     if(strcmp(argv[1], "plano") == 0)
         plano(atof(argv[2]), atof(argv[3]), argv[4]);
 }*/
-    box(4, 4, 4, 4, "caixinha.txt");
+    cone(2, 4, 6, 6, "caixinha.txt");
 }
