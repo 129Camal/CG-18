cfb6edff1d94fc3fd99846760433c464a195304e
elpaquito30 <filipe.v.f@hotmail.com>
2018-03-29 00:53:23 +0100

    parser quase feito(acho eu xD)

--- Parte2/motor/motor.cpp
+++ Parte2/motor/motor.cpp
@@ -7,9 +7,12 @@
 #pragma clang diagnostic push
 #pragma clang diagnostic ignored "-Wdeprecated-declarations"
 
+using namespace std;
+using namespace tinyxml2;
+
 float zz = -4, angleX=0.0, angleY=0.0;
 vector<Transforms> transformacoes;
-vector<Point> pontos;
+vector<Point> vertexes;
 
 void renderScene(void){
     // clear buffers
@@ -54,7 +57,193 @@ void reshape(int w, int h){
 }
 void keyboard(unsigned char key, int a, int b){}
 void keyboardspecial(int key, int a, int b){}
-void lerXML(string fich){}
+
+
+//método para ler o ficheiro e preencher vetor
+
+void readFile(string fich){
+    string linha;
+    string novo;
+    string delimiter = ",";
+    int pos;
+    float xx,yy,zz;
+    Point p ;
+
+    ifstream file(fich);
+
+    if(file.is_open()){
+
+        while(getline (file,linha)) {
+
+            pos = linha.find(delimiter);
+            novo = linha.substr(0, pos);
+            xx = atof(novo.c_str());
+            linha.erase(0, pos + delimiter.length());
+            p.x = xx;
+
+            pos = linha.find(delimiter);
+            novo = linha.substr(0, pos);
+            yy = atof(novo.c_str());
+            linha.erase(0, pos + delimiter.length());
+            p.y = yy;
+
+            pos = linha.find(delimiter);
+            novo = linha.substr(0, pos);
+            zz = atof(novo.c_str());
+            linha.erase(0, pos + delimiter.length());
+            p.z = zz;
+
+            vertexes.push_back(p);
+
+        }
+
+        file.close();
+
+
+    }
+    else {
+
+        cout << "ERRO AO LER FICHEIRO" << endl;
+    }
+}
+
+
+
+Transformacao PerformTransf(Translacao trans, Escala es, Rotacao rot, Transformacao transf){
+
+
+    Transformacao pt;
+
+    trans.setX(trans.getX() + transf.getTrans().getX());
+    trans.setY(trans.getY() + transf.getTrans().getY());
+    trans.setZ(trans.getZ() +  transf.getTrans().getZ());
+    es.setX(es.getX() * transf.getEscala().getX());
+    es.setY(es.getY() * transf.getEscala().getY());
+    es.setZ(es.getZ() * transf.getEscala().getZ());
+    rot.setX(rot.getAngle() + transf.getRotacao().getAngle());
+    rot.setY(rot.getX() + transf.getRotacao().getX());
+    rot.setY(rot.getY() + transf.getRotacao().getY());
+    rot.setZ(rot.getZ() + transf.getRotacao().getZ());
+
+
+    pt = Transformacao::Transformacao(trans,rot,es);
+
+    return pt;
+
+}
+
+//Parse do XML tendo em conta os níveis hierarquicos
+
+void Parser(XMLElement *group , Transformacao transf){
+
+    Transformacao trf;
+    Translacao trl;
+    Escala esc;
+    Rotacao rot;
+
+    float transX, transY, transZ, ang, esX, esY,esZ, rotX, rotY, rotZ;
+
+
+
+    if(strcmp(group->FirstChildElement()->Value(),"group")==0){
+        group = group->FirstChildElement();
+
+    }
+
+    XMLElement* transfor = group->FirstChildElement();
+
+    for(transfor; (strcmp(transfor->Value(),"models")!=0); transfor = transfor->NextSiblingElement()){
+        if(strcmp(transfor->Value(), "translate")==0){
+            if(transfor->Attribute("X")) transX = stof(transfor->Attribute("X"));
+            else transX = 0;
+            if(transfor->Attribute("Y")) transY = stof(transfor->Attribute("Y"));
+            else transY=0;
+            if(transfor->Attribute("Z")) transZ = stof(transfor->Attribute("Z"));
+            else transZ=0;
+            trl = Translacao::Translacao(transX,transY,transZ);
+
+        }
+        if(strcmp(transfor->Value(), "scale")==0){
+            if(transfor->Attribute("X")) esX = stof(transfor->Attribute("X"));
+            else esX=0;
+            if(transfor->Attribute("Y")) esY = stof(transfor->Attribute("Y"));
+            else esY=0;
+            if(transfor->Attribute("Z")) esZ = stof(transfor->Attribute("Z"));
+            else esZ=0;
+            esc.setX(esX);
+            esc.setY(esY);
+            esc.setZ(esZ);
+        }
+        if(strcmp(transfor->Value(), "rotate")==0){
+            if(transfor->Attribute("angle")) ang = stof(transfor->Attribute("angle"));
+            else ang=0;
+            if(transfor->Attribute("X")) rotX = stof(transfor->Attribute("X"));
+            else rotX =0;
+            if(transfor->Attribute("Y")) rotY = stof(transfor->Attribute("Y"));
+            else rotY=0;
+            if(transfor->Attribute("Z")) rotZ = stof(transfor->Attribute("Z"));
+            else rotZ=0;
+            rot = Rotacao::Rotacao(ang,rotX,rotY,rotZ);
+        }
+    }
+
+    trf= PerformTransf(trl,esc,rot,transf);
+
+    for(XMLElement* models = group->FirstChildElement("models")->FirstChildElement("model"); models; models = models -> NextSiblingElement("modelo")){
+        Transforms tran;
+        tran.setTipo(models->Attribute("fich"));
+        cout << tran.getTipo() << endl;
+        readFile(tran.getTipo());
+        tran.setPontos(vertexes);
+        vertexes.clear();
+
+        tran.setTrans(trf);
+
+        cout << "Translacao ->" << trf.getTrans().getX() << "-" << trf.getTrans().getY() << "-" << trf.getTrans().getZ() << endl;
+        cout << "Escala ->" << trf.getEscala().getX() << "-" << trf.getEscala().getY() << "-" << trf.getEscala().getZ() << endl;
+        cout << "Rotacao -> " << trf.getRotacao().getAngle() << "-" << trf.getRotacao().getX() << "-" << trf.getRotacao().getY() << "-" << trf.getRotacao().getZ() << endl;
+
+        transformacoes.push_back(tran);
+    }
+        // faz parse dos filhos
+    if(group->FirstChildElement("group")){
+            Parser(group->FirstChildElement("group"),trf);
+    }
+
+        //faz parse nos irmãos
+    if(group->NextSiblingElement("group")){
+        Parser(group->NextSiblingElement("group"),transf);
+
+    }
+
+
+
+
+}
+
+
+
+//método para ler ficheiro xml
+
+void lerXML(string fich) {
+    XMLDocument doc;
+
+
+
+    if (!(doc.LoadFile(fich.c_str()))) {
+
+            XMLElement * scene = doc.FirstChildElement("scene");
+            XMLElement * group = scene-> FirstChildElement("group");
+
+            Transformacao t = Transformacao::Transformacao();
+            Escala esc = Escala::Escala(0.5,0.5,0.5);
+            t.setEscala(esc);
+            Parser(group,t);
+        }else {
+        cout << "Ficheiro XML não foi encontrado" << endl;
+    }
+}
+
 
 
 int main(int argc, char** argv){
