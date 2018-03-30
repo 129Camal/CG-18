bcdeaa25059d38b480b3b7ea4726cdeb49ff8422
Sara Pereira <saralexx@gmail.com>
2018-03-30 18:58:16 +0100

    camara roda fixe, mas nao anda muito fixe. é perneta

--- Parte2/motor/motor.cpp
+++ Parte2/motor/motor.cpp
@@ -10,12 +10,77 @@
 using namespace std;
 using namespace tinyxml2;
 
-float xx = 0, yy = 0, zz = -4, angleX=0.0, angleY=0.0, lx = 6.0, ly = 6.0, lz = 6.0;
+float xx = 0, yy = 0, zz = -4, lxx = 0.0, lyy = 0.0, lzz = 0.0, angleX=0.0, angleY=0.0, angleZ = 0.0, lx = 30.0, ly = 30.0, lz = 30.0;
 
 int draw = GL_LINE;
 
 vector<Transforms> transformacoes;
 vector<Ponto> pontos;
+
+void drawAxis(){
+
+    // Draw mode of Axis's Arrow Heads (FILLED)
+    glPolygonMode(GL_FRONT_AND_BACK,draw);
+
+    // Arrow Head of X Axis (RED)
+    glRotatef(90.f,0,1,0); // Rotation of 90º Degrees, relative to the Y Axis... Now our Z axis, assumes the initial position of the X axis.
+    glTranslatef(0,0,5+xx); // Translation of "5+axes_x", relative to the Z Axis
+    glColor3f(1.0f,0.0f,0.0f); // Color of our X Arrow Head
+    glutSolidCone(0.1,0.3,5,5); // Arrow Head is drawn, as a solid cone. Base on Z=0, Height on Z=0.3, Radius of 0.3 {5 slices and 5 stacks}.
+    // This cone stays in the previously  translated position. In this case "5+axes_x"
+    glTranslatef(0,0,-5-xx); // Translates to the initial position - origin
+    glRotatef(-90.f,0,1,0); // Rotates to the initial position - origin
+
+    // Arrow Head of Y Axis (GREEN)
+    glRotatef(-90.f,1,0,0); // Rotation of -90º Degrees, relative to the X Axis... Now our Z axis, assumes the initial position of the Y axis.
+    glTranslatef(0,0,5+yy); // Translation of "5+axes_y", relative to the Z Axis
+    glColor3f(0.0f,1.0f,0.0f); // Color of our Y Arrow Head
+    glutSolidCone(0.1,0.3,5,5); // Arrow Head is drawn, as a solid cone. Base on Z=0, Height on Z=0.3, Radius of 0.3 {5 slices and 5 stacks}.
+    // This cone stays in the previously  translated position. In this case "5+axes_y"
+    glTranslatef(0,0,-5-yy); // Translates to the initial position - origin
+    glRotatef(90.f,1,0,0); // Rotates to the initial position - origin
+
+    // Arrow Head of Z Axis (BLUE)
+    glTranslatef(0,0,5+zz); // Translation of "5+axes_z", relative to the Z Axis
+    glColor3f(0.0f,0.0f,1.0f); // Color of our Z Arrow Head
+    glutSolidCone(0.1,0.3,5,5); // Arrow Head is drawn, as a solid cone. Base on Z=0, Height on Z=0.3, Radius of 0.3 {5 slices and 5 stacks}.
+    // This cone stays in the previously  translated position. In this case "5+axes_z"
+    glTranslatef(0,0,-5-zz); // Translates to the initial position - origin
+
+    glBegin(GL_LINES);
+
+    // Line of X Axis (RED) - From (0,0,0) to (5+axes_x,0,0)
+    glColor3f(1.0,0,0);
+    glVertex3f(0,0,0);
+    glVertex3f(5 + xx,0,0);
+
+    // Line of Y Axis (GREEN) - From (0,0,0) to (0,5+axes_y,0)
+    glColor3f(0,1.0,0);
+    glVertex3f(0,0,0);
+    glVertex3f(0,5 + yy,0);
+
+    // Line of Z Axis (BLUE) - From (0,0,0) to (0,0,5+axes_z)
+    glColor3f(0,0,1.0);
+    glVertex3f(0,0,0);
+    glVertex3f(0,0,5 + zz);
+
+    glEnd();
+
+    // Letter X coloring (RED) and positioning (5+axes_x+0.5,0,0)
+    glColor3f(1.0,0,0);
+    glRasterPos3f((5 + xx + 0.5),0,0);
+    glutBitmapCharacter(GLUT_BITMAP_HELVETICA_18,'X');
+
+    // Letter Y coloring (GREEN) and positioning (0,5+axes_y+0.5,0)
+    glColor3f(0,1.0,0);
+    glRasterPos3f(0,(5 + yy + 0.5),0);
+    glutBitmapCharacter(GLUT_BITMAP_HELVETICA_18,'Y');
+
+    // Letter Z coloring (BLUE) and positioning (0,0,5+axes_z+0.5)
+    glColor3f(0,0,1.0);
+    glRasterPos3f(0,0,(5 + zz + 0.5));
+    glutBitmapCharacter(GLUT_BITMAP_HELVETICA_18,'Z');
+}
 
 void renderScene(void){
     // clear buffers
@@ -25,17 +90,23 @@ void renderScene(void){
     glLoadIdentity();
     glPolygonMode(GL_FRONT_AND_BACK,draw);
 
-    gluLookAt(lx+20,ly+20,lz+60, //todo: alterar aqui
-              0.0,0.0,0.0,
+    gluLookAt(lx,ly,lz, //todo: alterar aqui
+              lxx,lyy,lzz,
               0.0f,1.0f,0.0f);
+
+
     glTranslatef(0, -1, zz);
 
     //draw instructions
     glRotatef(angleY, 0.0, 1.0, 0.0);
     glRotatef(angleX, 1.0, 0.0, 0.0);
-    glTranslatef(-xx,0,-yy);
+    glRotatef(angleZ, 0.0, 0.0, 1.0);
+    glTranslatef(-xx,-yy,-zz);
 
-    glBegin(GL_TRIANGLES);
+    drawAxis();
+
+
+
     int i;
     for(i = 0; i < transformacoes.size(); i++) {
         glPushMatrix();
@@ -58,7 +129,7 @@ void renderScene(void){
         glEnd();
         glPopMatrix();
     }
-    glEnd();
+
     glutSwapBuffers();
     //se fizermos o anel de saturno fica aqui
 }
@@ -83,6 +154,7 @@ void reshape(int w, int h){
     // return to the model view matrix mode
     glMatrixMode(GL_MODELVIEW);
 }
+
 void keyboard(unsigned char key, int a, int b) {
     switch (key) {
         case 'a':
@@ -95,22 +167,45 @@ void keyboard(unsigned char key, int a, int b) {
             angleY += 5;
             if (angleY > 360) angleY -= 360;
             break;
+        case 'q':
+        case 'Q':
+            angleX -= 5;
+            if (angleX < -360) angleX += 360;
+            break;
+
+        case 'e':
+        case 'E':
+            angleX += 5;
+            if (angleX > 360) angleX -= 360;
+            break;
         case 'w':
         case 'W':
-            if (angleX > -45) angleX -= 5;
-            if (angleX < -360) angleX += 360;
+            angleZ += 5;
+            if(angleZ > 360) angleZ += 360;
             break;
         case 's':
         case 'S':
-            if (angleX < 45) angleX += 5;
-            if (angleX > 360) angleX -= 360;
+            angleZ -= 5;
+            if(angleZ < -360) angleZ -=360;
             break;
         case '+':
-            glTranslatef(lx += 1, ly += 1, lz += 1);
+            glTranslatef(lx -= 1, ly -= 1, lz -= 1);
             break;
 
         case '-':
-            glTranslatef(lx -= 1, ly -= 1, lz -= 1);
+            glTranslatef(lx += 1, ly += 1, lz += 1);
+            break;
+        case 'f':
+        case 'F':
+            draw = GL_FILL;
+            break;
+        case 'p':
+        case 'P':
+            draw = GL_POINT;
+            break;
+        case 'l':
+        case 'L':
+            draw = GL_LINE;
             break;
     }
 }
@@ -118,20 +213,20 @@ void keyboard(unsigned char key, int a, int b) {
 void keyboardspecial(int key, int a, int b){
     switch (key){
         case GLUT_KEY_UP:
-            zz -=cos(angleY /180 * 3.141592653589793);
-            xx +=sin(angleY /180 * 3.141592653589793);
+            zz -=cos(angleY /180 * M_PI);
+            xx +=sin(angleY /180 * M_PI);
             break;
         case GLUT_KEY_DOWN:
-            zz +=1 * cos(angleY /180 * 3.141592653589793);
-            xx -=1 * sin(angleY /180 * 3.141592653589793);
+            zz +=1 * cos(angleY /180 * M_PI);
+            xx -=1 * sin(angleY /180 * M_PI);
             break;
         case GLUT_KEY_LEFT:
-            xx -=cos(angleY /180 * 3.141592653589793);
-            zz -=sin(angleY /180 * 3.141592653589793);
+            xx -=cos(angleY /180 * M_PI);
+            zz -=sin(angleY /180 * M_PI);
             break;
         case GLUT_KEY_RIGHT:
-            xx +=cos(angleY /180 * 3.141592653589793);
-            zz +=sin(angleY /180 * 3.141592653589793);
+            xx +=cos(angleY /180 * M_PI);
+            zz +=sin(angleY /180 * M_PI);
             break;
     }
 }
@@ -184,7 +279,6 @@ void readFile(string fich){
         cout << "ERRO AO LER FICHEIRO" << endl;
     }
 }
-
 
 
 Transformacao PerformTransf(Translacao trans, Escala es, Rotacao rot, Cor cor, Transformacao transf){
@@ -301,10 +395,10 @@ void Parser(XMLElement *group , Transformacao transf){
         pontos.clear();
         tran.setTrans(trf);
 
-        cout << "Translacao ->" << trf.getTrans().getX() << "-" << trf.getTrans().getY() << "-" << trf.getTrans().getZ() << endl;
+    /*   cout << "Translacao ->" << trf.getTrans().getX() << "-" << trf.getTrans().getY() << "-" << trf.getTrans().getZ() << endl;
         cout << "Escala ->" << trf.getEscala().getX() << "-" << trf.getEscala().getY() << "-" << trf.getEscala().getZ() << endl;
         cout << "Rotacao -> " << trf.getRotacao().getAngle() << "-" << trf.getRotacao().getX() << "-" << trf.getRotacao().getY() << "-" << trf.getRotacao().getZ() << endl;
-        cout << "Cor ->" << trf.getCor().getB() <<  "--" << trf.getCor().getG() << "-" << trf.getCor().getR()<<endl;
+        cout << "Cor ->" << trf.getCor().getB() <<  "--" << trf.getCor().getG() << "-" << trf.getCor().getR()<<endl;*/
         transformacoes.push_back(tran);
     }
         // faz parse dos filhos
